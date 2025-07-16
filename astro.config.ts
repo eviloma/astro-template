4e2cc81bdeeec6ac5df06074506ea688eabdac85
HighError <hierrorua@gmail.com>
2025-07-16 11:16:48 +0300

    feat: initial setup

---
+++ astro.config.ts
@@ -0,0 +1,41 @@
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import bun from "@hedystia/astro-bun";
+import compress from "@playform/compress";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import favicons from "astro-favicons";
+import icon from "astro-icon";
+import robots from "astro-robots";
+import vtbot from "astro-vtbot";
+
+export default defineConfig({
+  site: "http://localhost:4321",
+  adapter: bun(),
+  integrations: [
+    react(),
+    icon(),
+    vtbot({
+      loadingIndicator: false,
+    }),
+    favicons({
+      name: "Astro Basics",
+      short_name: "Astro Basics",
+      icons: {
+        favicons: true,
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        windows: true,
+        yandex: false,
+      },
+    }),
+    sitemap(),
+    robots(),
+    compress(),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
